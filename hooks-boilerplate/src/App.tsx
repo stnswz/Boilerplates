345bc4530cc4cc51f58e8a48c82b14267eb99636
Steffen Schwarz <stn_swz@gmx.net>
2021-04-16 18:53:53 +0200

    Adds boilerplate for hooks & sass

---
+++ hooks-boilerplate/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './css/App.scss';
+
+function App() {
+  return (
+    <>
+      <header className="App-header">
+        Header
+      </header>
+      <main>
+        Main
+      </main>
+    </>
+  );
+}
+
+export default App;
